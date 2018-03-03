c5f8c91874625d62131bd5e684f1aa0973032505
Robert Sumner Jr <rsumner31@gmail.com>
2018-03-03 13:57:49 -0600

    Add tox support
    tox aims to automate and standardize testing in Python. It is part of
    a larger vision of easing the packaging, testing and release process
    of Python software. [1]
    
    [1] http://tox.readthedocs.org/en/latest/
    
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>
    
    Conflicts:
    	setup.py
    	tox.ini

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+sys.path.insert(0, os.path.abspath('lib'))
+
+execfile('lib/ansiblelint/version.py')
+
+setup(
+    name='ansible-lint',
+    version=__version__,
+    description=('checks playbooks for practices and behaviour that could potentially be improved'),
+    keywords='ansible, lint',
+    author='Will Thames',
+    author_email='will@thames.id.au',
+    url='https://github.com/willthames/ansible-lint',
+    package_dir={'': 'lib'},
+    packages=find_packages('lib'),
+    zip_safe=False,
+    install_requires=['ansible', 'pyyaml'],
+    scripts=['bin/ansible-lint'],
+    license='MIT',
+    include_package_date=True,
+    test_suite="test"
+)

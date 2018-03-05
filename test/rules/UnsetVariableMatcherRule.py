221d6a7b94008feb1e05d6840b7dae530fbd514d
Robert Sumner Jr <rsumner31@gmail.com>
2018-03-05 01:18:22 -0600

    Made tag handling work using native sets
    Conflicts:
    	bin/ansible-lint
    	lib/ansiblelint/__init__.py
    	rules/DeprecatedTemplateBracketsRule.py
    	rules/GitHasVersionRule.py
    	rules/MercurialHasRevisionRule.py
    	rules/MismatchedBracketRule.py
    	rules/TrailingWhitespaceRule.py
    	test/rules/EMatcherRule.py
    	test/rules/UnsetVariableMatcherRule.py

---
+++ test/rules/UnsetVariableMatcherRule.py
@@ -0,0 +1,18 @@
+import ansiblelint.utils
+from ansiblelint import AnsibleLintRule
+
+class UnsetVariableMatcherRule(AnsibleLintRule):
+    ID = 'TEST0002'
+    SHORTDESC = 'Line contains untemplated variable'
+    DESCRIPTION = 'This is a test rule that looks for lines ' + \
+                  'post templating that still contain {{'
+    TAGS = {'fake', 'dummy', 'test2'}
+
+    def __init__(self):
+        super(self.__class__, self).__init__(id=self.ID, 
+                                             shortdesc=self.SHORTDESC,
+                                             description=self.DESCRIPTION,
+                                             tags=self.TAGS)
+
+    def match(self,playbook):
+        return ansiblelint.utils.matchlines(playbook, lambda x : "{{" in x)
